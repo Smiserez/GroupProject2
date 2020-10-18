3619c589c6a237cb57d44ff79bfcf826a2bf70d7
Smiserez <sdmiserez@gmail.com>
2020-10-18 13:31:06 -0500

    HALF DONE LOOK AT DISCORD FOR INFO

---
+++ ShootingGallery/Assets/Scripts/Target.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameObject target;
+
+    public GameObject effectsManager;
+    public GameObject hitEffect;
+    public float effectDuration = .1f;
+    
+    public AudioClip hitSound;
+    protected Effect effectScript;
+    void Awake()
+    {
+        effectScript = effectsManager.GetComponent<Effect>();
+    }
+
+    // Update is called once per frame
+    public virtual void Process(RaycastHit hit)
+    {
+        
+    }
+}
