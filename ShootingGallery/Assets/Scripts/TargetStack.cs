0ee58dfcddfe86aadaca45d38871760a6e7ca180
wthompson2 <wthompson@unomaha.edu>
2020-10-19 18:52:24 -0500

    Added trees, cube stack, and diamond

---
+++ ShootingGallery/Assets/Scripts/TargetStack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TargetStack : Target
+{
+    public float impactForce;
+
+    Rigidbody targetRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRigidbody = target.GetComponent<Rigidbody>();
+    }
+
+    public override void Process(RaycastHit hit)
+    {
+        targetRigidbody.AddForce(-hit.normal * impactForce);
+
+        effectScript.Play(hit, hitSound, hitEffect, effectDuration);
+    }
+}
